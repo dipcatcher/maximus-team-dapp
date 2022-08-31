5cb3ebbb78a36452c97861e93ec5cad3702c1db8
oaadmin@protonmail.com <oaadmin@protonmail.com>
2022-08-31 19:51:45 +0000

    Created Main_copy_old and IMG_1128.PNG; Edited Main_copy, settings, disclaimer and chain_interface

---
+++ client_code/Main_copy_old/__init__.py
@@ -0,0 +1,91 @@
+from ._anvil_designer import Main_copy_oldTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+import webbrowser
+import time
+
+from ..dashboard_page import dashboard_page
+
+from ..offline_dashboard import offline_dashboard
+
+from ..mint_page_copy import mint_page_copy
+from ..redeem_page_copy import redeem_page_copy
+from ..dashboard_page_copy import dashboard_page_copy
+
+from ..disclaimer import disclaimer
+
+try:
+  from anvil.js.window import ethers, ethereum
+except:
+  pass
+
+import anvil.js
+anvil.js.report_all_exceptions(False, reraise=False)
+
+
+
+print(anvil.server.get_app_origin())
+
+
+class Main_copy_old(Main_copy_oldTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.address=None
+    
+    
+    
+
+
+
+
+
+  def button_connect_dapp_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    
+    alert(disclaimer(), large=True, dismissible=False, buttons=[])
+  
+  def menu_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    b = event_args['sender'].text
+    if b=='Mint':
+     
+      self.content_panel.clear()
+      
+      try:
+        self.m = mint_page_copy(main=self)
+        
+        self.content_panel.add_component(self.m)
+      except:
+        pass
+
+
+    if b=='Redeem':
+      self.content_panel.clear()
+      
+      self.content_panel.add_component(redeem_page_copy(main=self))
+      
+
+    if b =='Dashboard':
+      self.content_panel.clear()
+      try:
+        #self.d = dashboard_page(main=self)
+        
+        self.d=offline_dashboard(main=self)
+        self.content_panel.add_component(self.d)
+      except Exception as e:
+        raise e
+    if b=='Calculator':
+      self.content_panel.clear()
+      self.c=calculator_page()
+      self.content_panel.add_component(self.c)
+
+  
+
+
+
--- client_code/disclaimer/__init__.py
+++ client_code/disclaimer/__init__.py
@@ -17,27 +17,24 @@ class disclaimer(disclaimerTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    
 
     # Any code you write here will run when the form opens.
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    
     self.raise_event('x-close-alert', value=True)
-    if is_ethereum:
-      
-      
-     
+    if is_ethereum:     
       try:
         a = ethereum.request({
                   'method': 'wallet_switchEthereumChain',
-                  'params': [{ "chainId": '0x1' }]
+                  'params': [{ "chainId": '0x3AD' }] #'0x1
               })
         anvil.js.await_promise(a)
       except:
         pass
-      anvil.js.window.location.replace("https://go.maximus.cash")
-      
+      self.clear()
+      open_form('Main')
     else:
       Notification('To use the dapp you must be on an etherem enabled browser, such as MetaMask.').show()
       try:
